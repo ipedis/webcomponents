99609d2fd894a5c27afa5815b19cf63c344690f9
MarioIpedis <mario@ipedis.com>
2026-02-05 13:18:49 +0400

    💄upgrade design

--- apps/demo/src/app/pages/home/home.component.ts
+++ apps/demo/src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Tooltip1Component } from '../tooltip/tooltip1/tooltip1.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, Tooltip1Component],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
