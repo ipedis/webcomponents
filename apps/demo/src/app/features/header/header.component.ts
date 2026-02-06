3ba04e6e677e06e3c8c832fdd7d4e1e73e1c99bb
MarioIpedis <167958544+MarioIpedis@users.noreply.github.com>
2026-02-06 14:19:16 +0400

    💄upgrade design (#70)
    * 💄upgrade design
    
    * feat(docker): upgrade Node.js version to 24 and add staging deployment workflows
    
    * 💄upgrade design
    
    * feat(docker): upgrade Node.js version to 24 and add staging deployment workflows (#71)
    
    * feat(ci): rename jobs and update Dockerfile paths for PDF accessibility demo
    
    * 🗑️clean code
    
    * 💄upgrade design
    
    * 🗑️clean code
    
    ---------
    
    Co-authored-by: Yanis <yanis.ghidouche@gmail.com>

--- apps/demo/src/app/features/header/header.component.ts
+++ apps/demo/src/app/features/header/header.component.ts
@@ -1,21 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  constructor(
-    private router: Router,
-  ) { }
-
-  goToTooltip() {
-    this.router.navigate(['/tooltip']);
-  }
+  constructor(private router: Router) {}
 }
