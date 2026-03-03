307bb30e66208d6f89a263c614fb5819fd764f95
Romaric <romaric@ipedis.com>
2026-03-03 11:36:53 +0300

    feat(transloco): add server loader for translation management

--- apps/demo/src/app/app.config.server.ts
+++ apps/demo/src/app/app.config.server.ts
@@ -1,9 +1,14 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { TRANSLOCO_LOADER } from '@jsverse/transloco';
 import { appConfig } from './app.config';
+import { TranslocoServerLoader } from './transloco.server.loader';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [
+    provideServerRendering(),
+    { provide: TRANSLOCO_LOADER, useClass: TranslocoServerLoader },
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
